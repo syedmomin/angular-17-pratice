2b42d8925997148b185c3f31fae3c11caadf94ad
syed momin <syedmomin168@gmail.com>
2024-10-10 22:07:56 +0500

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tryfitters';
-}
+export class AppComponent {}
